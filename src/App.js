2e18d536ead8ed125f94baccade41f86333b889b
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-24 13:52:37 -0800

    add basic mobile friendly layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <p>hello world</p>
+      <div className="container">15 Minutes
+        <p>A new way to search Twitter. 
+          <br/>How will you use your 15 minutes? 
+          <br/>Select ‘Go to Showcase’ to browse tweets from celebrities using their fame for good.</p>
+      </div>
+      <div className="buttonsContainer">
+        <button className="searchBtn"><a>Search Tweets</a></button>
+        <button className="homeBtn"><a>Home</a></button>
+        <button className="showcaseBtn"><a>Go to Showcase</a></button>
+      </div>
     </div>
   );
 }
