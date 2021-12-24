4ccf6583aaac854b75c181d979db84f688393cef
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-24 14:14:55 -0800

    fix background image lightness

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="container">15 Minutes
-        <p>A new way to search Twitter. 
-          <br/>How will you use your 15 minutes? 
-          <br/>Select ‘Go to Showcase’ to browse tweets from celebrities using their fame for good.</p>
+      <div className="container">
+        <div className="contentContainer">
+            <h1>15 Minutes</h1>
+            <p>A new way to search Twitter. 
+              <br/>How will you use your 15 minutes? 
+              <br/>Select ‘Go to Showcase’ to browse tweets from celebrities using their fame for good.</p>
+          </div>
       </div>
       <div className="buttonsContainer">
         <button className="searchBtn"><a>Search Tweets</a></button>
