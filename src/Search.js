a66d03688f987eb3b1ff852c8ffe7c4f7200d502
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-24 16:54:51 -0800

    add style to search screen

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import './index.css'
+import TV from './pics/TV.png'
 
 export default function Search() {
     return (
-        <div>
-            <h1>Search screen</h1>
+        <div className="searchContainer"
+            style={{
+                backgroundColor: 'red',
+                width: '100vw',
+                height: '100vh'
+                // backgroundColor: 'rgba(255, 251, 251, 0.3)',
+                // backgroundImage: `url(${TV})`,
+                // backgroundBlendMode: 'lighten',
+            }}
+        >
+            <div className="searchBtnsContainer">
+                <button className="searchBtnUser">Search by User</button>
+                <button className="searchBtnContent">Search by Content</button>
+                <input type="text" placeholder="Type search item here..."></input>
+            </div>
+            <div className="searchResultContainer"></div>
         </div>
     )
 }
