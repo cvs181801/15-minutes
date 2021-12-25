7827009b46a1825f51be8e5091b15478a9ee2b8a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-25 11:36:26 -0800

    add a test array file for testing the data rendering.

---
+++ src/test.js
@@ -0,0 +1,20 @@
+const test = [
+    {
+        username: "Jake",
+        text: "lorem ipsum ;olijalekdijacns isu,danfl;djlnfiv udjlsacisjd!!!",
+        image: '#',
+        video: '#',
+        gif: '#',
+        retweetCount: 4,
+        favoritedCount: 17
+    },
+    {
+        username: "SallyLoo",
+        text: "lorem ipsum ;olijalekdijacns isu,danfl;djlnfiv udjlsacisjd!!!",
+        image: '#',
+        video: '#',
+        gif: '#',
+        retweetCount: 6,
+        favoritedCount: 207 
+    }
+]
