4558a45c8deb63e89e1ae30f4219d0cda5c25820
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-24 16:03:18 -0800

    set  up react router basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-
 import './App.css';
-
+import {Link, Router, Switch, Route} from "react-router-dom"
+import Showcase from './Showcase'
+import Search from './Search'
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <div className="container">
         <div className="contentContainer">
             <h1>15 Minutes</h1>
-            <p>A new way to search Twitter. 
-              <br/>How will you use your 15 minutes? 
-              <br/>Select ‘Go to Showcase’ to browse tweets from celebrities using their fame for good.</p>
+            <p>A new way to search Twitter.
+            <br/> 
+            <br/>
+            How will you use your 15 minutes? 
+            <br/>
+            <br/>
+            Select ‘Go to Showcase’ to browse tweets from celebrities using their fame for good.</p>
           </div>
       </div>
       <div className="buttonsContainer">
-        <button className="searchBtn"><a>Search Tweets</a></button>
-        <button className="homeBtn"><a>Home</a></button>
-        <button className="showcaseBtn"><a>Go to Showcase</a></button>
+        <button className="searchBtn"><Link to="/search">Search Tweets</Link></button>
+        <button className="homeBtn"><Link to="/">Home</Link></button>
+        <button className="showcaseBtn"><Link to="/showcase">Go to Showcase</Link></button>
+
+        <Switch>
+            <Route path="/search"><Search /></Route>
+            <Route path="/"><App /></Route>
+            <Route path="/showcase"><Showcase /></Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
