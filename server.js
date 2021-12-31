8011fc566a5d3d4b8ede186814890e9ecca34e49
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-31 12:01:12 -0800

    add a client file and adjust file pasths in the methods in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3001
 app.use('/', express.static(path.join(__dirname, "client")));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + 'client/build/index.html'))
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 app.listen(port, () => {
