57666362cce6e26914111066fb1632a801c05e88
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-30 18:03:36 -0800

    fix the paramaters for app.use in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const port = 3001
 
-app.use('/build', express.static(path.join(__dirname, "build")));
+app.use('/', express.static(path.join(__dirname, "build")));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/build/index.html'))
