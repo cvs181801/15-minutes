3163244f65b9ae8648b841e522249d1850a49e36
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-12-31 11:46:11 -0800

    create a client folder and move src, build, public and static in there

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express()
 const path = require('path')
 const port = 3001
 
-app.use('/', express.static(path.join(__dirname, "build")));
+app.use('/', express.static(path.join(__dirname, "client")));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'))
+  res.sendFile(path.join(__dirname + 'client/build/index.html'))
 })
 
 app.listen(port, () => {
