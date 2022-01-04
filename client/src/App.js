fe4855c0dc099e831a29f6e383551aa76f5c503a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-01-03 17:15:58 -0800

    add basic nav bar style

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,44 @@
 import './index.css';
+import React, {useState} from 'react'
 import {BrowserRouter, Link, Router, Route, Routes} from "react-router-dom"
 import Showcase from './Showcase'
 import Search from './Search'
 import Home from './Home'
 
 function App() {
+
+const [color, setColor] = useState('black')
+
   return (
     <BrowserRouter>
     <div className="App">
      
       <div className="buttonsContainer">
-        <Link to="/search">Search Tweets</Link>  {/*<button className="searchBtn"></button> */}
-        <Link to="/">Home</Link>  {/*<button className="homeBtn"></button> */}
-        <Link to="/showcase">Go to Showcase</Link>  {/*<button className="showcaseBtn"></button> */}
+        <Link to="/search"
+          style={{
+            color: `${color}`,
+            textDecoration: 'none'
+          }}
+          onMouseEnter={()=>setColor('magenta')}
+          onMouseLeave={()=>setColor('black')}
+          
+        >Search Tweets</Link>  
+        <Link to="/"
+          style={{
+            color: `${color}`,
+            textDecoration: 'none'
+          }}
+          onMouseEnter={()=>setColor('magenta')}
+          onMouseLeave={()=>setColor('black')}
+        >Home</Link>  
+        <Link to="/showcase"
+          style={{
+            color: `${color}`,
+            textDecoration: 'none'
+          }}
+          onMouseEnter={()=>setColor('magenta')}
+          onMouseLeave={()=>setColor('black')}
+        >Go to Showcase</Link>  
 
         <Routes>
             <Route path="/search" element={<Search />} />
